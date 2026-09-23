a6b3d7400d70c0e7cbc081073603829ec386723d
bharath-kumar-venukanti <bharath-kumar-venukanti@users.noreply.github.com>
2026-09-23 10:44:30 +0300

    Reorganize operator and statement samples into topic folders, add switch statement example
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ 06_JS_Statements/JS_Switch/JS_Switch_statement_sample.js
@@ -0,0 +1,28 @@
+let dayNumber = 3;
+let dayName;
+
+switch(dayNumber){
+    case 1:
+        dayName="Monday";
+       // console.log(dayName+","+dayNumber);
+        break;
+    case 2:
+        dayName="Tuesday";
+       // console.log(dayName+","+dayNumber);
+        break;
+    case 3:
+        dayName="Wednesday";
+       //console.log(dayName+","+dayNumber);
+        break;
+    case 3:
+        dayName="Thursday";
+       //console.log(dayName+","+dayNumber);
+        break;
+    case 4:
+        dayName="Friday";
+       //console.log(dayName+","+dayNumber);
+        break;
+    default:
+            console.log("Weekend and invalid to work");
+}
+console.log(`Day is ${dayNumber} and it is ${dayName}`)
