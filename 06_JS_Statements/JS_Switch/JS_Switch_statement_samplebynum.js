3726cbe007da42d28815374a1c147c807e0acef1
bharath-kumar-venukanti <bharath-kumar-venukanti@users.noreply.github.com>
2026-09-23 18:22:21 +0300

    Code Updated

--- 06_JS_Statements/JS_Switch/JS_Switch_statement_samplebynum.js
+++ 06_JS_Statements/JS_Switch/JS_Switch_statement_samplebynum.js
@@ -1,4 +1,30 @@
-let dayNumber = 3;
+let dayNumber = 3;  
+// switch (dayNumber) {
+//     case 0:
+//         console.log("Sunday - Rest Day");
+//         break;
+//     case 1:
+//         console.log("Monday - Sprint Planning");
+//         break;
+//     case 2:
+//         console.log("Tuesday - Development");
+//         break;
+//     case 3:
+//         console.log("Wednesday - Code Review");
+//         break;
+//     case 4:
+//         console.log("Thursday - Testing");
+//         break;
+//     case 5:
+//         console.log("Friday - Deployment & Retro");
+//         break;
+//     case 6:
+//         console.log("Saturday - Rest Day");
+//         break;
+//     default:
+//         console.log("Invalid day value");
+//     }
+
 let dayName;
 
 switch(dayNumber){
@@ -23,6 +49,6 @@ switch(dayNumber){
        //console.log(dayName+","+dayNumber);
         break;
     default:
-            console.log("Weekend and invalid to work");
+            console.log("Weekends are not allowed to work");
 }
 console.log(`Day is ${dayNumber} and it is ${dayName}`)
